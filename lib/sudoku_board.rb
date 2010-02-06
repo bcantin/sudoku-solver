e18f01b089304ec6699e4e42061ae7b91eae1757
Brad Cantin <bcantin@spatialnetworks.com>
2010-02-06 12:01:56 -0500

    created cell class

--- lib/sudoku_board.rb
+++ lib/sudoku_board.rb
@@ -1,29 +1,53 @@
 class SudokuBoard
   
-  attr_accessor :rows
-  attr_accessor :cols
-  attr_accessor :board
+  attr_accessor :cells
   
   Arry = [1,2,3,4,5,6,7,8,9]
   
   def initialize
-    @board = {}
+    @cells = []
     generate_board_cells
   end
   
   def generate_board_cells
     Arry.each do |row|
       Arry.each do |col|
-        Cell.new(row, col)
+        @cells << Cell.new(row, col)
       end
     end
   end
   
+  ValueAlreadyExistsError = Class.new(RuntimeError)
+  
+  def set_cell_value(row,column,value)
+    cell = get_cell_value(row,column)
+    raise ValueAlreadyExistsError if cell.value != nil
+    cell.value = value
+    cell
+  end
+  
+  def get_cell_value(row,column)
+    @cells.each do |c|
+    if c.row == row && c.column == column
+       return c
+     end
+    end
+  end
 end
 
 class Cell
   
-  def initialize(row, column)
-     @row, @column = row, column
+  attr_reader :row, :column
+  attr_accessor :value, :guesses
+  
+  def initialize(row, column, val=nil)
+    @val = val if val
+    @row, @column = row, column
+    @value = val if val
+    @guesses = []
+  end
+  
+  def to_s
+    "{:row => #{@row}, :column => #{@column}, :value => #{@value}}"
   end
 end
--- spec/sudoku/sudoku_board_spec.rb
+++ spec/sudoku/sudoku_board_spec.rb
@@ -2,8 +2,46 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "sudoku_board" do
 
-  it "a board should have 81 cells" do
-    board = SudokuBoard.new
-    # board.rows.size.should == 9
+  before(:each) do
+    @board = SudokuBoard.new
   end
+
+  it "a board should have 81 cells" do
+    @board.cells.size.should == 81
+  end
+  
+  it "should be able to set a cell value" do
+    @board.set_cell_value(1,2,5).value.should == 5
+  end
+  
+  describe "indivudual cell interaction" do
+    before(:each) do
+      @board.set_cell_value(1,2,5)
+    end
+    it "should be able to get the value of a cell" do
+      cell = @board.get_cell_value(1,2)
+      cell.value.should == 5
+    end
+    
+    it "should not be able to set a cell value if the value is not nil" do
+      lambda { @board.set_cell_value(1,2,6) }.should raise_error
+    end
+  end
+
+end
+
+describe "cells" do
+  
+  before(:each) do
+    @cell = Cell.new(1,2)
+  end
+  it "should have a row and a column" do
+    @cell.row.should == 1
+    @cell.column.should == 2
+  end
+  
+  it "the value should default to nil" do
+    @cell.value.should == nil
+  end
+
 end
